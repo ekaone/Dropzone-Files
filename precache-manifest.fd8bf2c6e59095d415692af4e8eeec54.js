32668166797b5ad095a092ae66cdccfe7c097fc8
Eka Prasetia <ekaone3033@gmail.com>
2020-06-19 19:20:00 +0700

    Updates

---
+++ precache-manifest.fd8bf2c6e59095d415692af4e8eeec54.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "fb1bffcaff562a47e904f7abc82d76de",
+    "url": "/Dropzone-Files/index.html"
+  },
+  {
+    "revision": "22389643b7c19137139e",
+    "url": "/Dropzone-Files/static/css/main.200e37c2.chunk.css"
+  },
+  {
+    "revision": "bd9226ee4bf6c478babd",
+    "url": "/Dropzone-Files/static/js/2.53464b05.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/Dropzone-Files/static/js/2.53464b05.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "22389643b7c19137139e",
+    "url": "/Dropzone-Files/static/js/main.036ba4aa.chunk.js"
+  },
+  {
+    "revision": "408e3006082f055fbad7",
+    "url": "/Dropzone-Files/static/js/runtime-main.8653f2e3.js"
+  }
+]);
